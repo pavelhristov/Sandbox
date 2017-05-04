ca65e4674a9ef3f284cbf43624d8e15ab929c98b
pavelhristov <pave.hristov_@abv.bg>
2017-05-04 17:27:57 +0300

    custom view engine

---
+++ CmsWannabe/CmsWannabe/Extensions/CustomViewEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsWannabe.Extensions
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine()
+        {
+            //WidgetRegistry.Init();
+            //var folders = WidgetRegistry.WidgetNames();
+
+            var viewLocations = new List<string>{
+            "~/Views/{1}/{0}.aspx",
+            "~/Views/{1}/{0}.ascx",
+            "~/Views/{1}/{0}.cshtml",
+            "~/Views/Shared/{0}.aspx",
+            "~/Views/Shared/{0}.ascx",
+            "~/Views/Shared/{0}.cshtml",
+            "~/AnotherPath/Views/{0}.ascx",
+            "~/Views/Shared/Templates/{0}.cshtml"
+            // etc
+            };
+
+            //foreach (var item in folders)
+            //{
+            //    viewLocations.Add("~/Views/" + item + "/{0}.cshtml");
+            //}
+
+
+            this.PartialViewLocationFormats = viewLocations.ToArray();
+            this.ViewLocationFormats = viewLocations.ToArray();
+        }
+    }
+}
